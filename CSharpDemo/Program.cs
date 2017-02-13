67dbe4596bf2333e8354c2eb0bf0a70bcb88ecde
hongshuai <hjmrezl@outlook.com>
2017-02-13 09:43:49 +0800

    派生类的重写版本

--- CSharpDemo/Program.cs
+++ CSharpDemo/Program.cs
@@ -1,56 +1,18 @@
 ﻿using System;
 
-class Rectangle
+public class Shape
 {
-	protected double length;
-	protected double width;
-
-	public Rectangle(double l, double w)
+	public int X { get; private set; }
+	public int Y { get; private set; }
+	public int Height { get; set; }
+	public int Width { get; set; }
+	public virtual void Draw()
 	{
-		length = l;
-		width = w;
-	}
-
-	public double GetArea()
-	{
-		return length * width;
-	}
-
-	public void Display()
-	{
-		Console.WriteLine("长度为:{0}", length);
-		Console.WriteLine("宽度为:{0}", width);
-		Console.WriteLine("面积为:{0}", GetArea());
+		Console.WriteLine("Performing base class drawing tasks");
 	}
 }
 
-class Tabletop : Rectangle
+class Circle : Shape
 {
-	private double cost;
 
-	public Tabletop(double l, double w) : base(l, w)
-	{}
-
-	public double GetCost()
-	{
-		double cost;
-		cost = GetArea() * 70;
-		return cost;
-	}
-
-	public void Display()
-	{
-		base.Display();
-		Console.WriteLine("成本为:{0}", GetCost());
-	}
-}
-
-class RectangleTest
-{
-	public static void Main(string[] args)
-	{
-		Tabletop t = new Tabletop(4.5, 7.5);
-		t.Display();
-		Console.ReadLine();
-	}
 }
